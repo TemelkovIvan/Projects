9e5b8652c8a1bc77e34519be8f4e83d55c4f1b9f
Ivan Temelkov <93338868+TemelkovIvan@users.noreply.github.com>
2022-01-18 18:10:16 +0200

    Delete Spring Boot with HTML info for Design Patterns directory

--- Spring Boot with HTML info for Design Patterns/Design Patterns/src/main/java/education/springbootcontroller/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package education.springbootcontroller;
-
-import org.springframework.data.repository.CrudRepository;
-/*
-public interface UsersRepository extends CrudRepository<Users, Long> {
-}
-*/
